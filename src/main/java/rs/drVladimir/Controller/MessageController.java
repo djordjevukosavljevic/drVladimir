5a107e6e377dd8d98fae66579e0e9a1cb9da80f1
djordjevukosavljevic <djordje.vukosavljevic01@gmail.com>
2025-11-14 18:01:40 +0100

    added message controller

--- src/main/java/rs/drVladimir/Controller/MessageController.java
+++ src/main/java/rs/drVladimir/Controller/MessageController.java
@@ -1,10 +1,14 @@
 package rs.drVladimir.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import rs.drVladimir.Entity.Message;
 import rs.drVladimir.Service.MessageService;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(path = "/api/message")
@@ -16,5 +20,17 @@ public class MessageController
     public MessageController(MessageService messageService)
     {
         this.messageService = messageService;
+    }
+
+    @GetMapping
+    public List<Message> getAllMessages()
+    {
+        return messageService.getAllMessages();
+    }
+
+    @GetMapping(path = "/{id}")
+    public Optional<Message> findMessageById(Integer id)
+    {
+        return messageService.findMessageById(id);
     }
 }
