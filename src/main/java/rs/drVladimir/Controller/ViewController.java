de80e5b42ca57b6d0739a51b3087f767391381d5
djordjevukosavljevic <djordje.vukosavljevic01@gmail.com>
2025-11-25 15:30:39 +0100

    added app.properties to gitignore still nothing

--- src/main/java/rs/drVladimir/Controller/ViewController.java
+++ src/main/java/rs/drVladimir/Controller/ViewController.java
@@ -1,9 +1,14 @@
 package rs.drVladimir.Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ViewController
 {
-
+    @GetMapping
+    public String defaultView()
+    {
+        return "index";
+    }
 }
