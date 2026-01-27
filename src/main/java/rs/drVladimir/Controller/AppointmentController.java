6fdebc1c0f9b684d3de1b226010d87a133aa8010
djordjevukosavljevic <djordje.vukosavljevic01@gmail.com>
2026-01-27 16:38:26 +0100

    added crud functions into AppointmentServices

--- src/main/java/rs/drVladimir/Controller/AppointmentController.java
+++ src/main/java/rs/drVladimir/Controller/AppointmentController.java
@@ -18,4 +18,6 @@ public class AppointmentController
         this.service = service;
     }
 
+
+
 }
--- src/main/java/rs/drVladimir/Service/AppointmentService.java
+++ src/main/java/rs/drVladimir/Service/AppointmentService.java
@@ -2,7 +2,11 @@ package rs.drVladimir.Service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import rs.drVladimir.Entity.Appointment;
 import rs.drVladimir.Repository.AppointmentRepository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class AppointmentService
@@ -16,5 +20,18 @@ public class AppointmentService
         this.repository = repository;
     }
 
+    public List<Appointment> getAllAppointments()
+    {
+        return repository.findAll();
+    }
 
+    public Optional<Appointment> findAppointmentById(Integer id)
+    {
+        return repository.findById(id);
+    }
+
+    public void deleteAppointmentById(Integer id)
+    {
+        repository.deleteById(id);
+    }
 }
